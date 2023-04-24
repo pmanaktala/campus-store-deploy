8fcd79aa3300f6089efed0fb636ad81991058647
Parth Manaktala <manaktala.parth@gmail.com>
2023-04-24 19:55:55 -0400

    Added Cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cors from "cors";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -17,6 +18,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.originalUrl);
